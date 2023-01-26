d85464402b94c80f369d6773c26011f06135c722
sungyun <sungyun@kaeri.re.kr>
2023-01-26 11:22:00 +0900

    button 태그를 js 객체로 불러서 event 를 부여하니 작동 X 방법 있을듯

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Button from "./Button";
+import styled from "./App.module.css";
+import { useState } from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("render");
   return (
     <div>
-      <h1>Hello</h1>
+      <h1 className={styled.title}>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
