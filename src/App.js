45ab2a1fde3c6837f8b86c6e88da66bd03702cda
sungyun <sungyun@kaeri.re.kr>
2023-01-26 16:59:17 +0900

    to-do-list 작성

--- src/App.js
+++ src/App.js
@@ -2,25 +2,51 @@ import Button from "./Button";
 import styled from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function Hello() {
-  function destroyedFn() {
-    console.log("bye");
-  }
-  function effectFn() {
-    console.log("created");
-    return destroyedFn;
-  }
-  useEffect(effectFn, []);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  const deleteClick = (event) => {
+    event.preventDefault();
+    const idx = event.target.id;
+    console.log(idx);
+    setToDos((currentArray) =>
+      currentArray.filter((item) => {
+        return item != idx;
+      })
+    );
+  };
+
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length}) </h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your To-Do"
+        ></input>
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      {toDos.map((item, index) => (
+        <form key={index}>
+          <li className={styled.list}>{item}</li>
+          <button id={item} onClick={deleteClick}>
+            delete
+          </button>
+        </form>
+      ))}
     </div>
   );
 }
