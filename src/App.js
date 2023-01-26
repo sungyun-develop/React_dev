99974debe1e224ccf0e3e7055c0d9e8f46bb86ef
sungyun <sungyun@kaeri.re.kr>
2023-01-26 11:29:31 +0900

    State가 변하더라도 한번만 코드를 실행하고 싶은 함수는 useEffect 사용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Button from "./Button";
 import styled from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("render");
+  console.log("i run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("i run only once");
+  };
+  useEffect(iRunOnlyOnce, []);
   return (
     <div>
       <h1 className={styled.title}>{counter}</h1>
