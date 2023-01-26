9251d3da16c1f7a85352b30ff1d3383e6a8400e6
sungyun <sungyun@kaeri.re.kr>
2023-01-26 15:42:50 +0900

    state에서 특정 키워드가 변경될때만 실행하도록하는 useEffect

--- src/App.js
+++ src/App.js
@@ -4,14 +4,32 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time");
+  const onChange = (event) => setKeyword(event.target.value);
+
   const iRunOnlyOnce = () => {
     console.log("i run only once");
   };
   useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    console.log("when 'keyword' changes");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("when 'counter' changes");
+  }, [counter]);
+  useEffect(() => {
+    console.log("when 'counter' & 'keyword' changes");
+  }, [counter, keyword]);
+
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1 className={styled.title}>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
